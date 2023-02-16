5151dd40b29bc7aa453fdf639a6ddb921540f21d
Ádám Szabadi <szabadiadam@hotmail.com>
2023-02-16 15:13:27 +0100

    Add DI to LoggerLibrary & register LoggerFactory as scoped service

---
+++ LoggerLibrary/Extensions/LoggerLibraryExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace LoggerLibrary.Extensions
+{
+    using LoggerLibrary._Abstraction;
+    using LoggerLibrary.Factory;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class LoggerLibraryExtensions
+    {
+        public static IServiceCollection AddLoggerServices(this IServiceCollection services)
+        {
+            services.AddScoped<ILoggerFactory, LoggerFactory>();
+            return services;
+        }
+    }
+}
